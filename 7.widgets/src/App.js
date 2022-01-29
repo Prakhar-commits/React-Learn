77bb3d8271bc8a0ddc8cf57be95e1288caac3b85
Drish <drish.xd@gmail.com>
2022-01-29 14:06:58 +0530

    Hooks app

--- 7.widgets/src/App.js
+++ 7.widgets/src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
-// import Search from "./components/Search";
-// import Accordion from "./components/Accordion";
-// import Dropdown from "./components/Dropdown";
+import Search from "./components/Search";
+import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
+import Navbar from "./components/Navbar";
+import { Route } from "./components/Route";
 
 const items = [
   {
@@ -43,13 +45,30 @@ const App = () => {
   const [showDropDown, setShowDropDown] = useState(true);
 
   return (
-    <div className="ui container">
-      {/* <Accordion items={items} /> */}
-      {/* <Search /> */}
-      {/* <button onClick={() => setShowDropDown(!showDropDown)}>Toogle Dropdown </button> */}
-      {/* {showDropDown ? <Dropdown options={options} selected={selected} onSelectedChange={setSelected} label="Select Colors" /> : null} */}
-      <Translate />
-    </div>
+    <>
+      <Navbar />
+      <div className="ui container">
+        <Route path="/">
+          <h2>Accordion</h2>
+          <Accordion items={items} />
+        </Route>
+
+        <Route path="/search">
+          <h2>Search</h2>
+          <Search />
+        </Route>
+
+        <Route path="/dropdown">
+          <h2>Dropdowm</h2>
+          <button onClick={() => setShowDropDown(!showDropDown)}>Toogle Dropdown </button>
+          {showDropDown ? <Dropdown options={options} selected={selected} onSelectedChange={setSelected} label="Select Colors" /> : null}
+        </Route>
+
+        <Route path="/translate">
+          <Translate />
+        </Route>
+      </div>
+    </>
   );
 };
 
